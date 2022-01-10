bcdc77f68c6f7d42d9caed6273022028d47203c7
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-01-10 17:02:27 -0300

    first_commit main.dart extruture

--- main.dart
+++ main.dart
@@ -1,3 +1,28 @@
+import 'dart:io';
+
 void main() {
-  print("Hello World!");
+  print("Welcome to the show!");
+
+  while (true) {
+    print("[L]ist  [I]nsert  [E]dit  [C]all  [D]elete  e[X]it");
+    stdout.write("Enter your option: ");
+    String option = stdin.readLineSync()!;
+
+    if ("Ll".contains(option)) {
+      print("List patients");
+    } else if ("Ii".contains(option)) {
+      print("Insert patient");
+    } else if ("Ee".contains(option)) {
+      print("Edit patient");
+    } else if ("Cc".contains(option)) {
+      print("Calling patient...");
+    } else if ("Dd".contains(option)) {
+      print("Delete");
+    } else if ("Xx".contains(option)) {
+      print("Exit");
+      break;
+    } else {
+      print("Unknown option. Please, try again.");
+    }
+  }
 }
